fb8ae3aee408db31e1d931617f3e214b3906ae6c
zaporter <opensource@zackporter.com>
2023-03-23 21:31:15 -0400

    edit main

--- orchestrator/src/main.rs
+++ orchestrator/src/main.rs
@@ -1,18 +1,21 @@
 
+use anyhow::{anyhow, Context, Result};
 use git2::{Cred, FetchOptions, RemoteCallbacks, Repository};
 use std::path::Path;
 use std::time::Duration;
 use tokio::time::sleep;
+use log::{debug, error, info, trace, warn};
+use env_logger::{Builder, Env};
 
-const REPO_URL: &str = "https://github.com/zaporter/rdk";
-const LOCAL_PATH: &str = "../sample2";
+const REPO_URL: &str = "https://github.com/zaporter/co2meter-rs";
+const LOCAL_PATH: &str = "./sample4";
 
-async fn clone_and_pull_updates() {
+async fn clone_and_pull_updates() -> Result<()> {
+    info!("Cloning repo: {REPO_URL}");
     // Clone the repository
-    let repo = match Repository::clone(REPO_URL, Path::new(LOCAL_PATH)) {
-        Ok(repo) => repo,
-        Err(e) => panic!("Failed to clone repository: {}", e),
-    };
+    let repo = Repository::clone(REPO_URL, Path::new(LOCAL_PATH))
+        .with_context(|| format!("Failed to clone repository: {}", REPO_URL))?;
+    info!("Finished cloning");
 
     // Prepare the remote callbacks with credentials
     let mut callbacks = RemoteCallbacks::new();
@@ -25,33 +28,48 @@ async fn clone_and_pull_updates() {
     fetch_options.remote_callbacks(callbacks);
 
     loop {
+        info!("Sleeping for 60 seconds");
         // Wait for a minute before pulling updates
         sleep(Duration::from_secs(60)).await;
 
         // Fetch and merge updates
-        if let Err(e) = repo.find_remote("origin") {
-            println!("Failed to find remote 'origin': {}", e);
-            continue;
-        }
-        let mut remote = repo.find_remote("origin").unwrap();
+        let mut remote = match repo.find_remote("origin") {
+            Ok(remote) => remote,
+            Err(e) => {
+                println!("Failed to find remote 'origin': {}", e);
+                continue;
+            }
+        };
         if let Err(e) = remote.fetch(&["refs/heads/main"], Some(&mut fetch_options), None) {
             println!("Failed to fetch updates: {}", e);
             continue;
         }
 
-        let fetch_head = repo.find_reference("FETCH_HEAD").unwrap();
-        let fetch_commit = repo.reference_to_annotated_commit(&fetch_head).unwrap();
-        let analysis = repo.merge_analysis(&[&fetch_commit]).unwrap();
+        let fetch_head = match repo.find_reference("FETCH_HEAD") {
+            Ok(reference) => reference,
+            Err(e) => {
+                println!("Failed to find FETCH_HEAD reference: {}", e);
+                continue;
+            }
+        };
+        let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)
+            .with_context(|| "Failed to get annotated commit for FETCH_HEAD")?;
+        let analysis = repo.merge_analysis(&[&fetch_commit])
+            .with_context(|| "Failed to perform merge analysis")?;
 
         if analysis.0.is_up_to_date() {
             println!("Already up to date.");
         } else if analysis.0.is_fast_forward() {
             println!("Fast-forwarding...");
             let refname = format!("refs/heads/main");
-            let mut reference = repo.find_reference(&refname).unwrap();
-            reference.set_target(fetch_commit.id(), "Fast-Forward").unwrap();
-            repo.set_head(&refname).unwrap();
-            repo.checkout_head(None).unwrap();
+            let mut reference = repo.find_reference(&refname)
+                .with_context(|| format!("Failed to find reference: {}", refname))?;
+            reference.set_target(fetch_commit.id(), "Fast-Forward")
+                .with_context(|| "Failed to set target for reference")?;
+            repo.set_head(&refname)
+                .with_context(|| format!("Failed to set HEAD to reference: {}", refname))?;
+            repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))
+                .with_context(|| "Failed to checkout HEAD")?;
         } else {
             println!("Merge required, not supported in this example.");
         }
@@ -59,6 +77,7 @@ async fn clone_and_pull_updates() {
 }
 
 #[tokio::main]
-async fn main() {
-    clone_and_pull_updates().await;
+async fn main() -> Result<()> {
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+    clone_and_pull_updates().await
 }
